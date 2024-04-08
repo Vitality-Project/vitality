946d7a90e072097d3b2fb18031ec160a47c21c77
Isabella Sofia Martins <106943011+ibellmartins@users.noreply.github.com>
2024-04-08 03:08:23 -0300

    Add files via upload

---
+++ vitalityN1/vitality/app/cadastro/cadastro.js
@@ -0,0 +1,10 @@
+
+export default function Cadastro(){
+    return(
+        <main>
+            <h1>
+              oi
+            </h1>
+        </main>
+    );
+}
---
+++ vitalityN1/vitality/app/page.js
@@ -0,0 +1,10 @@
+
+import Login from "./login/login";
+
+export default function Home() {
+  return (
+    <main>
+      <Login/>
+    </main>
+  );
+}
